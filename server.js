786d9bed8cb9bba7d26248ccf3419b9a8950aa4f
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-07-27 00:20:11 +1000

    api update

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+require('dotenv').config();
 const bodyParser = require("body-parser")
 const connectDB = require('./database.js')
 const FHB = require(('./model.js'))
