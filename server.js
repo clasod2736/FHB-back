cd54293e6f2b6e61583487820bfc37e002eae16b
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-09-29 10:44:29 +1000

    fixing axios...

--- server.js
+++ server.js
@@ -186,8 +186,8 @@ app.post("/register", async function (req, res) {
 
 // POST user data for login
 app.post("/login", async (req, res) => {
-  const userEmail = req.query.email;
-  const userPassword = req.query.password;
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
 
   try {
     const userInfo = await FHB.findOne({
