28b2296fc780f2bf1cba1573cd6f915d4029a423
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-09-29 11:31:40 +1000

    changed httpOnly

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get("/isAuth", (req, res) => {
         res.cookie("accessToken", newAccessToken, {
           domain: "https://main--voluble-kashata-776f36.netlify.app/",
           secure: true,
-          httpOnly: true,
+          httpOnly: false,
         });
 
         res.send(decodedRefresh).sendStatus(200);
@@ -210,12 +210,12 @@ app.post("/login", async (req, res) => {
         res.cookie("accessToken", accessToken, {
           domain: "https://main--voluble-kashata-776f36.netlify.app/",
           secure: true,
-          httpOnly: true,
+          httpOnly: false,
         });
         res.cookie("refreshToken", refreshToken, {
           domain: "https://main--voluble-kashata-776f36.netlify.app/",
           secure: true,
-          httpOnly: true,
+          httpOnly: false,
         });
         res.status(200);
         console.log(userInfo.email, "LoggedIn!");
