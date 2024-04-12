f03239109442c6c6490b71557043ca806d427069
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-12 22:20:11 +1000

    change to Schema.ObjectId

--- model/userData.js
+++ model/userData.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
 
 const FHBSchema = new Schema({
   _id: {
-    type: Types.ObjectId,
+    type: ObjectId,
   },
   email: {
     type: String,
