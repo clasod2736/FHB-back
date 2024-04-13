05eed5df5e9f0287216def9e4ed938f96cb469a4
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-13 13:58:39 +1000

    test

--- api/user/logIn.js
+++ api/user/logIn.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
-const jwtUtils = require("../jwt/jwt");
+const jwtUtils = require("../../jwt/jwt");
 const { send } = require("express/lib/response.js");
 
 router.post("/login", async (req, res) => {
