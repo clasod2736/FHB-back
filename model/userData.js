e37471b1f52eb44ddafc1556635f44fb41b126e7
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-13 22:11:30 +1000

    deleted _id in Schema

--- model/userData.js
+++ model/userData.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
 
 const FHBSchema = new Schema({
-  _id: {
-    type: ObjectId,
-  },
   email: {
     type: String,
   },
