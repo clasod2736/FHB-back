dba92777724b7fe4cbc7c703440ac80d96bb77e1
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-13 23:41:53 +1000

    fixed recentbrew api

--- api/brewInfo/getRecentbrew.js
+++ api/brewInfo/getRecentbrew.js
@@ -6,6 +6,11 @@ router.get("/getRecentbrew", async (req, res) => {
   try {
     const user = await FHB.findOne({ email: req.query.email });
 
+    if (user.oldBrews.length < 1) {
+      res.sendStatus(404);
+      return;
+    }
+
     const sortedBrews = user.oldBrews.sort((a, b) => b.order - a.order);
     const recentbrew = sortedBrews[0];
 
