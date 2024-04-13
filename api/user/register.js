e1b7cc27528439d16a8fe5271606bfd6660c5606
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-13 21:38:30 +1000

    added FHB userData

--- api/user/register.js
+++ api/user/register.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const FHB = require("../../model/userData");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async function (req, res) {
   const userEmail = req.body.email;
